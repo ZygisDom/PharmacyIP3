97b2727ebcea77d502d7a4ee6432f42340a747ca
SamBr228 <56259937+SamBr228@users.noreply.github.com>
2020-03-03 15:06:15 +0000

    Login System
    .sql file included for database if needed

---
+++ pharmacydb.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2020 at 04:02 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pharmacydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `ID` int(10) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`ID`, `username`, `password`) VALUES
+(1, 'JSmith', 'jsmithpass'),
+(2, '', 'testpass');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ processlogin.php
@@ -0,0 +1,37 @@
+<html>
+<body>
+
+<?php
+    // Start a new session
+    session_start();
+
+    // Retrieve username and password submitted by user
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Establishing connection to the database
+    $conn = mysqli_connect("localhost", "root", "", "pharmacydb");
+    mysqli_select_db($conn, "pharmacydb");
+
+    // Protection against malicious SQL injection
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    // SQL Query to compare submitted data to the database
+    $result = mysqli_query($conn,"select * from staff where username = '$username' and password= '$password'") or die("Database query failed".mysqli_error());
+    $row = mysqli_fetch_array($result);
+    if ($row['username'] == $username && $row['password'] == $password){
+        echo "Login successful";
+    } else {
+        echo "Login failed.";
+    }
+
+    $_SESSION["username"] = $username;
+    $_SESSION["password"] = $password;
+    mysqli_close($conn);
+    header("Location: /pharmacy/home.php");
+    ?>
+</body>
+</html>
