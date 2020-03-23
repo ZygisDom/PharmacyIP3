302435eb82de742960fcd69755403a475e5c0dbb
Zygimantas <zygis.domarkas@gmail.com>
2020-03-23 20:14:25 +0000

    Fixed add new patient, added modal to the Stock Management, corrected option values

--- js.js
+++ js.js
@@ -34,14 +34,12 @@ function GetSelectedValue1() {
     const f = document.getElementById("value1");
     const result = f.options[f.selectedIndex].value;
 
-
-
     const e = document.getElementById("value2");
     const result2 = e.options[e.selectedIndex].value;
 
+    var h = "Total amount: ";
 
-
-    const g = (+result + +result2)*50;
+    const g = h + (+result + +result2)*50;
     document.getElementById("result3").innerHTML = g;
 
 }
